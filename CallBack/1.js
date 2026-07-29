9cc51adb8ab00791592b7fc4efab32fa87893405
Saniya-2007 <25075A0521@vnrvjiet.in>
2026-07-29 08:02:19 +0530

    callbacks

--- CallBack/1.js
+++ CallBack/1.js
@@ -14,4 +14,43 @@ function calculator(a,b,sum1,sub1){
  console.log(sub1(a,b));
 }
 calculator(10,5,sum,sub);
+/*
+sum(10, 20, (a, b) => {
+    console.log(a + b);
+});
 
+*/
+//callback hell
+function getData(dataId, getNextData) {
+
+    setTimeout(() => {
+
+        console.log("data", dataId);
+
+        if (getNextData) {
+            getNextData();
+        }
+
+    }, 2000);
+}
+
+getData(1, () => {
+
+    console.log("getting data2...");
+
+    getData(2, () => {
+
+        console.log("getting data3...");
+
+        getData(3, () => {
+
+            console.log("getting data4...");
+
+            getData(4);
+
+        });
+
+    });
+
+});
+/*This deeply nested structure is called callback hell (also known as the pyramid of doom). Modern JavaScript usually avoids this by using Promises and async/await, which keep asynchronous code much flatter and easier to follow.*/
